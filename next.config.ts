0f3d8682eff0e24ceedf3dd825e0707339370942
subfield <tetelainv@gmail.com>
2025-07-13 16:55:39 +0100

    build(next): disable strict mode and ignore build errors
    Disable React strict mode and configure TypeScript and ESLint to ignore build errors to streamline development process

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
